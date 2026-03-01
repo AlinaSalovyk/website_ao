4787a9601b5f5702aa335451e11abf0a3996b6b2
denys_matsevych <matsevychd@gmail.com>
2026-03-01 19:01:23 +0200

    refactor: extract Logo SVG into reusable component\n\nCreated src/components/icons/Logo.tsx with currentColor fills,\nrole=\"img\" and aria-label for accessibility.\nReplaced inline SVG in MainLayout.tsx with the new component."

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 ﻿import { useState, type ReactNode, type JSX } from "react";
 import { Menu } from "../routes/Menu/Menu";
+import { Logo } from "../components/icons/Logo";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -36,31 +37,7 @@ export const MainLayout = ({ children, variant = "default", customLogo, headerPo
               {customLogo ? (
                 customLogo
               ) : (
-                <svg
-                  width="83"
-                  height="32"
-                  viewBox="0 0 83 32"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-10 w-auto"
-                >
-                  <path
-                    d="M45.6854 0.0372467H13.75V8.37522H45.6854V0.0372467Z"
-                    fill="white"
-                  />
-                  <path
-                    d="M25.452 13.0111H13.6533V21.3491H25.452V31.9728H33.79V21.3491H45.5887V13.0111H33.79H25.452Z"
-                    fill="white"
-                  />
-                  <path
-                    d="M81.4999 8.37522V0.0372467H49.5645V31.9727H81.4999V13.011H57.9185V8.37522H81.4999ZM73.1619 21.349V23.6186H57.9185V21.349H73.1619Z"
-                    fill="white"
-                  />
-                  <path
-                    d="M9.83797 0.0372467H1.5V31.9727H9.83797V0.0372467Z"
-                    fill="white"
-                  />
-                </svg>
+                <Logo />
               )}
             </a>
           </div>
